7204824ba823fc7d3ff2dc45f6169e41c9f7c0ca
SIB2 <eric091214@gmail.com>
2026-04-04 02:24:48 +0900

    파일 디렉터리 잘 못 옮김 + 오늘 배운 내용

--- workspace_java/_chap13/CollectionExam.java
+++ workspace_java/_chap13/CollectionExam.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CollectionExam {
     static void main(String[] args) {
@@ -65,6 +67,50 @@ public class CollectionExam {
         System.out.println("=== 비어있는가? ===");
         System.out.println(list.isEmpty());
 
+        System.out.println("--------------");
 
+        Map map = new HashMap();
+
+        // 추가 - 변수에 담을 수 있는 모든 것을 넣을 수 있다.
+        map.put("key", "value");
+        map.put("k2", 123);
+        map.put("k3", list);
+
+        map.put("k2", 321); // 키가 없는 경우
+                            //      Key 추가
+                            // 키가 있는 경우
+                            //      Key 덮어쓰기
+
+        String map_str = (String)map.get("key");
+        System.out.println(map_str);
+
+        System.out.println(map.get("가사")); // 없으면 NULL
+
+        // ------------------------------------------ //
+
+        list = new ArrayList();
+        map = new HashMap();
+
+//        map.put("한로로", "사랑하게 될꺼야");
+        map.put("가수명", "한로로");
+        map.put("제목", "사랑하게 될꺼야");
+
+        // 얕은 복사로 들어간다.
+        list.add(map);
+        System.out.println(list);
+        System.out.println(":+:+:+:+:+:+:+:+:");
+        map.put("가수명", "kiiikiii");
+
+        System.out.println(list);
+
+        // 제네릭
+        // 노란줄 꼴보기 싫어서 쓴다네용 ㅎ. zㅋ
+
+        // 추가할 자료형을 제한한다. !!
+
+        // ⁂ 전달인자나 리턴타입의 자료형을 동적으로 변경한다.
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("string", 1234);
+        System.out.println(map1);
     }
 }
